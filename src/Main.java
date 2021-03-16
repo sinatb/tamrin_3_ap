57090cbe1bc8ebcc2414ffbb5aa3cd4d41796d9a
sina <sinatb.dev@gmail.com>
2021-03-16 19:43:11 +0330

    this is the first commit to master branch

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String [] args){
+        System.out.println("Hello git");
+        System.out.println("My name is: <your name>");
+        System.out.println("My student ID is <your id>");
+    }
+}
