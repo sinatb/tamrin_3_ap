d5ab1568578b989e6244bf1e3971379dc1f8905a
sina <sinatb.dev@gmail.com>
2021-03-16 19:46:49 +0330

    added my name to the master branch

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String [] args){
         System.out.println("Hello git");
-        System.out.println("My name is: <your name>");
+        System.out.println("My name is: sina taheri behrooze");
         System.out.println("My student ID is <your id>");
     }
 }
